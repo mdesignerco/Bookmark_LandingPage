e45d92dafaca0984511c5a1a2bfe8a7bb2ac9383
J Hernandez <jmcgraphics90@gmail.com>
2020-07-20 00:58:40 -0500

    update email validation, half css menu

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -5,10 +5,33 @@ const tabActive = document.getElementsByClassName('active');
 const accordionOpen = document.getElementsByClassName('open');
 const accordion = document.getElementsByClassName('features__accordion__item');
 const accordionItem = document.getElementsByClassName('features__collapse');
+const buttonRegister = document.getElementById('registerButton');
+const inputEmail = document.getElementById('register');
+const registerGroup = document.getElementById('registerGroup');
+const logo = document.getElementById('logo');
+
 let width = screen.availWidth;
 
-if( width <= '1024') {
+let errorMessage = `
+  <input
+    id="register"
+    type="text"
+    placeholder="Enter your email address"
+    required
+  />
+  <span class="input-message">Whoops, make sure it's an email</span>`;
 
+let confirmationMessage = `
+  <input
+    id="register"
+    type="text"
+    placeholder="Enter your email address"
+    required
+  />
+  <span class="input-message">Great!, You email has been send </span>`;
+
+if( width <= '1024') {
+  logo.src = './Assets/images/logo-bookmark-white.svg';
 } else {
   document.onreadystatechange = () => {
     let margin = 80;
@@ -19,20 +42,19 @@ if( width <= '1024') {
   };
 }
 
-
-
-
 for ( let i = 0; i < accordion.length; i++ ) {
   accordion[i].addEventListener('click', () => {
     removeClass(accordionOpen, 'open');
-    addClass(accordion[i], accordionItem[i] ,'open');
+    addClass( accordion[i],'open' );
+    addClass( accordionItem[i] ,'open' );
   });
 };
 
 for ( let i = 0; i < tab.length; i++ ) {
   tab[i].addEventListener ('click', () => {
       removeClass(tabActive, 'active');
-      addClass(tab[i],tabContent[i], 'active');
+      addClass(tab[i], 'active');
+      addClass(tabContent[i], 'active');
   });
 };
 
@@ -45,7 +67,20 @@ const removeClass = ( elementItem, className ) => {
   };
 };
 
-const addClass = (elementHead, elementItem, className) => {
+const addClass = (elementHead, className) => {
   elementHead.classList.add(className);
-  elementItem.classList.add(className);
 };
+
+const validateEmail = () => {
+  let expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  let email = inputEmail.value;
+  if (!expr.test(email)) {
+    addClass(registerGroup, 'input__group-error');
+    registerGroup.innerHTML = errorMessage;
+  } else {
+    addClass( registerGroup, 'input__group-great');
+    registerGroup.innerHTML = confirmationMessage;
+  };
+};
+
+buttonRegister.addEventListener ('click', validateEmail );
